52a67b937b0bbda22991d20ff0e7cae331c01c65
Karan Garg <karangarg01001@gmail.com>
2016-07-25 11:56:11 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,9 +5,10 @@ import os
 if __name__ == "__main__":
 
     output_file_path = os.path.join(os.getcwd(), "test_video.swf")
+    vnc_file_path = os.path.join(os.getcwd(), "vnc_passwd")
     
     #Create a Castro video object
-    video = Castro(filename = output_file_path, passwd = "vnc_passwd")
+    video = Castro(filename = output_file_path, passwd = vnc_file_path)
 
     #start screen recording
     print "\nStarting 10-second screen capture...\n"
